ed3e42ddf0d5d176aa36e2837a3a2d139a676b51
Justin <juc81@pitt.edu>
2022-12-21 21:33:23 -0500

    ground, collision objects, and basic movement

---
+++ TopDown2D/Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    public float collisionOffset = 0.05f;
+
+    public ContactFilter2D movementFilter;
+    
+
+    Vector2 movementInput;
+    Rigidbody2D rb;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        //If movement input is not 0, try to move
+        if(movementInput != Vector2.zero) {
+            // Check for potential collisions
+            int count = rb.Cast(
+                movementInput, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+                movementFilter, // The settings that determine where a collision can occur
+                castCollisions, // List of collisions to store the found collisions into after Cast is finished
+                moveSpeed * Time.fixedDeltaTime + collisionOffset // Amount to cast equal to movement plus an offset -> avoid getting stuck in wall
+            );
+            if(count == 0) {
+                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            }
+        }
+    }
+
+    void OnMove(InputValue movementValue) {
+        movementInput = movementValue.Get<Vector2>();
+    }
+}
